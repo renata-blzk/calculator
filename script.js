ea81ddc5656264a8aa0331a725b3288560e495e6
renata-blzk <renata.balzekas@gmail.com>
2025-01-05 12:53:24 +0000

    create basic functions for +, -, *, /

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const header = document.querySelector('h1')
 header.style.color = 'pink';
 
+const add = (a, b) => {
+    return (a + b)
+}
+
+const substract = (a, b) => {
+    return (a - b);
+}
+
+const multiply = (a, b) => {
+    return (a * b);
+}
+
+const divide = (a, b) => {
+    return (a / b);
+}
+console.log(divide(45, 2));
