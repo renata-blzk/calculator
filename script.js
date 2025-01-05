d48eeb025ac714c378e05cfce3628b615711e089
renata-blzk <136249041+renata-blzk@users.noreply.github.com>
2025-01-05 13:53:13 +0000

    add function operate

--- script.js
+++ script.js
@@ -20,3 +20,18 @@ const divide = (a, b) => {
 let num1;
 let num2;
 let operator;
+
+const operate = (num1, operator, num2) => {
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return divide(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+    }
+};
+
+
